edf270e4aa982185817c35cce2a904b4e7271c2a
jonnypui <43037228+jonnypui@users.noreply.github.com>
2020-01-20 10:06:18 -0500

    More options & first two budget buttons all work now

--- budgetfiles/budget.js
+++ budgetfiles/budget.js
@@ -212,6 +212,7 @@ function subtractauto() {
 function sethome() {
   if (i == 0) {
     i = 1;
+    var homepercent = document.getElementById("homepercent");
     var elem = document.getElementById("homebar");
     elem.style.backgroundColor = "#42f554";
     var status = document.getElementById("status");
@@ -224,7 +225,7 @@ function sethome() {
       if (homewidth == value) {
         clearInterval(id);
         i = 0;
-      } else if (value > 100) {
+      } else if (value > 200) {
         status.innerHTML = " The Value is too large!";
         clearInterval(id);
         i = 0;
@@ -237,32 +238,36 @@ function sethome() {
         if((homewidth+1) < value){
           homewidth = homewidth + 1;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
         else{
           homewidth = homewidth + 0.01;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
       }else if (homewidth > value) {
         if((homewidth-1) > value){
           homewidth = homewidth - 1;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
         else{
           homewidth = homewidth - 0.01;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
       }
-      if (homewidth == "100") {
+      if (homewidth == "200") {
         elem.style.backgroundColor = "red";
-        status.innerHTML = " Maxed spending on Home!!!";
+        status.innerHTML = " Maxed spending on Home Utilities!!!";
       }
     }
 
@@ -271,66 +276,73 @@ function sethome() {
 
 function addhome() {
   var value = parseFloat(document.getElementById("hometext").value);
-    value = Math.floor((value) * 100) / 100;
-    value = value + homewidth;
+
+  value = Math.floor((value) * 100) / 100;
+  value = value + homewidth;
   if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("homebar");
-    elem.style.backgroundColor = "#42f554";
-    var status = document.getElementById("status");
-    status.innerHTML = "";
-    console.log("The Set Value: " + value)
-    var id = setInterval(frame, 1);
-    function frame() {
-      if (homewidth == value) {
-        clearInterval(id);
-        i = 0;
-      } else if (value > 100) {
-        status.innerHTML = " The Value is too large!";
-        clearInterval(id);
-        i = 0;
-      } else if (value < 0) {
-        status.innerHTML = " The Value must be bigger than $0!";
-        clearInterval(id);
-        i = 0;
+  i = 1;
+  var homepercent = document.getElementById("homepercent");
+  var elem = document.getElementById("homebar");
+  elem.style.backgroundColor = "#42f554";
+  var status = document.getElementById("status");
+  status.innerHTML = "";
+  console.log("The Set Value: " + value)
+  var id = setInterval(frame, 1);
+  function frame() {
+    if (homewidth == value) {
+      clearInterval(id);
+      i = 0;
+    } else if (value > 200) {
+      status.innerHTML = " The Value is too large!";
+      clearInterval(id);
+      i = 0;
+    } else if (value < 0) {
+      status.innerHTML = " The Value must be bigger than $0!";
+      clearInterval(id);
+      i = 0;
+    }
+    else if (homewidth < value) {
+      if((homewidth+1) < value){
+        homewidth = homewidth + 1;
+        homewidth = Math.round((homewidth) * 100) / 100;
+        elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
+        elem.innerHTML = "$" + homewidth;
+        homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
       }
-      else if (homewidth < value) {
-        if((homewidth+1) < value){
-          homewidth = homewidth + 1;
-          homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
-          elem.innerHTML = "$" + homewidth;
-        }
-        else{
-          homewidth = homewidth + 0.01;
-          homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
-          elem.innerHTML = "$" + homewidth;
-        }
-      }else if (homewidth > value) {
-        if((homewidth-1) > value){
-          homewidth = homewidth - 1;
-          homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
-          elem.innerHTML = "$" + homewidth;
-        }
-        else{
-          homewidth = homewidth - 0.01;
-          homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
-          elem.innerHTML = "$" + homewidth;
-        }
+      else{
+        homewidth = homewidth + 0.01;
+        homewidth = Math.round((homewidth) * 100) / 100;
+        elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
+        elem.innerHTML = "$" + homewidth;
+        homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
       }
-      if (homewidth == "100") {
-        elem.style.backgroundColor = "red";
-        status.innerHTML = " Maxed spending on Home!!!";
+    }else if (homewidth > value) {
+      if((homewidth-1) > value){
+        homewidth = homewidth - 1;
+        homewidth = Math.round((homewidth) * 100) / 100;
+        elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
+        elem.innerHTML = "$" + homewidth;
+        homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
+      }
+      else{
+        homewidth = homewidth - 0.01;
+        homewidth = Math.round((homewidth) * 100) / 100;
+        elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
+        elem.innerHTML = "$" + homewidth;
+        homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
       }
     }
-
+    if (homewidth == "200") {
+      elem.style.backgroundColor = "red";
+      status.innerHTML = " Maxed spending on Home Utilities!!!";
+    }
   }
+
+}
 }
 
 function subtracthome() {
+  var homepercent = document.getElementById("homepercent");
   var value = parseFloat(document.getElementById("hometext").value);
     value = Math.floor((value) * 100) / 100;
     value = value - homewidth;
@@ -347,7 +359,7 @@ function subtracthome() {
       if (homewidth == value) {
         clearInterval(id);
         i = 0;
-      } else if (value > 100) {
+      } else if (value > 200) {
         status.innerHTML = " The Value is too large!";
         clearInterval(id);
         i = 0;
@@ -365,32 +377,36 @@ function subtracthome() {
         else if((homewidth+1) < value){
           homewidth = homewidth + 1;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
         else{
           homewidth = homewidth + 0.01;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
       }else if (homewidth > value) {
         if((homewidth-1) > value){
           homewidth = homewidth - 1;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
         else{
           homewidth = homewidth - 0.01;
           homewidth = Math.round((homewidth) * 100) / 100;
-          elem.style.width = homewidth + "%";
+          elem.style.width = ((homewidth/homemax)* 100).toFixed(2) + "%";
           elem.innerHTML = "$" + homewidth;
+          homepercent.innerHTML = ((homewidth / homemax) * 100).toFixed(2);
         }
       }
-      if (homewidth == "100") {
+      if (homewidth == "200") {
         elem.style.backgroundColor = "red";
-        status.innerHTML = " Maxed spending on Home!!!";
+        status.innerHTML = " Maxed spending on home Insurance!!!";
       }
     }
 
